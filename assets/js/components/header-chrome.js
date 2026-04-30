f4c84220a3db112047de68a345d935bd4acfa3ac
Mark Holmes <contact@markholm.es>
2026-04-30 07:54:17 -0500

    Better hiding

--- assets/js/components/header-chrome.js
+++ assets/js/components/header-chrome.js
@@ -3,96 +3,100 @@ export function headerChrome() {
     toolbarVisible: true,
     searchOpen: false,
     clearToolbarHideTimer() {
-      clearTimeout(this._toolbarTimer)
-      this._toolbarTimer = null
+      clearTimeout(this._toolbarTimer);
+      this._toolbarTimer = null;
     },
     scheduleToolbarHide() {
-      this.clearToolbarHideTimer()
+      this.clearToolbarHideTimer();
       this._toolbarTimer = setTimeout(() => {
         if (!this.searchOpen) {
-          this.toolbarVisible = false
-          this.dispatchToolbarVisibility()
+          this.toolbarVisible = false;
+          this.dispatchToolbarVisibility();
         }
-      }, 3000)
+      }, 3000);
     },
     dispatchToolbarVisibility() {
-      window.dispatchEvent(new CustomEvent('toolbar:visibility', {
-        detail: { visible: this.toolbarVisible }
-      }))
+      window.dispatchEvent(
+        new CustomEvent("toolbar:visibility", {
+          detail: { visible: this.toolbarVisible },
+        }),
+      );
     },
     init() {
-      const isPostPage = Boolean(document.getElementById('post-content'))
+      const isPostPage = Boolean(document.getElementById("post-content"));
       const updateToolbarVisibility = () => {
         if (!isPostPage) {
-          this.clearToolbarHideTimer()
-          this.toolbarVisible = true
-          this.dispatchToolbarVisibility()
-          return
+          this.clearToolbarHideTimer();
+          this.toolbarVisible = true;
+          this.dispatchToolbarVisibility();
+          return;
         }
 
-        const main = document.querySelector('main')
-        const isAboveThreshold = !main || main.getBoundingClientRect().top > 0
-        const scrollY = window.scrollY
-        const isScrollingUp = scrollY < (this._lastScrollY ?? scrollY)
-        this._lastScrollY = scrollY
+        const main = document.querySelector("main");
+        const isAboveThreshold = !main || main.getBoundingClientRect().top > 0;
+        const scrollY = window.scrollY;
+        const isScrollingUp = scrollY < (this._lastScrollY ?? scrollY);
+        this._lastScrollY = scrollY;
 
-        this.clearToolbarHideTimer()
+        this.clearToolbarHideTimer();
 
-        if (this.searchOpen) {
-          this.toolbarVisible = true
-          this.dispatchToolbarVisibility()
-          return
+        if (this.searchOpen || isAboveThreshold) {
+          this.toolbarVisible = true;
+          this.dispatchToolbarVisibility();
+          return;
         }
 
-        if (isAboveThreshold || isScrollingUp || this.toolbarVisible) {
-          this.toolbarVisible = true
-          this.dispatchToolbarVisibility()
-          this.scheduleToolbarHide()
+        if (isScrollingUp || this.toolbarVisible) {
+          this.toolbarVisible = true;
+          this.dispatchToolbarVisibility();
+          this.scheduleToolbarHide();
         }
-      }
+      };
 
-      updateToolbarVisibility()
-      window.addEventListener('scroll', updateToolbarVisibility, { passive: true })
-      window.addEventListener('resize', updateToolbarVisibility)
-      window.addEventListener('search:open', (event) => {
-        this._scrollYBeforeSearch = event.detail?.scrollY ?? window.scrollY
-        this._toolbarVisibleBeforeSearch = this.toolbarVisible
-        this._toolbarTimerWasActiveBeforeSearch = Boolean(this._toolbarTimer)
-        this.clearToolbarHideTimer()
-        this.searchOpen = true
-        this.toolbarVisible = true
-        this.dispatchToolbarVisibility()
-      })
-      window.addEventListener('search:close', (event) => {
-        this.clearToolbarHideTimer()
-        this.searchOpen = false
+      updateToolbarVisibility();
+      window.addEventListener("scroll", updateToolbarVisibility, {
+        passive: true,
+      });
+      window.addEventListener("resize", updateToolbarVisibility);
+      window.addEventListener("search:open", (event) => {
+        this._scrollYBeforeSearch = event.detail?.scrollY ?? window.scrollY;
+        this._toolbarVisibleBeforeSearch = this.toolbarVisible;
+        this._toolbarTimerWasActiveBeforeSearch = Boolean(this._toolbarTimer);
+        this.clearToolbarHideTimer();
+        this.searchOpen = true;
+        this.toolbarVisible = true;
+        this.dispatchToolbarVisibility();
+      });
+      window.addEventListener("search:close", (event) => {
+        this.clearToolbarHideTimer();
+        this.searchOpen = false;
 
-        const scrollYBeforeSearch = this._scrollYBeforeSearch ?? window.scrollY
-        const currentScrollY = event.detail?.scrollY ?? window.scrollY
+        const scrollYBeforeSearch = this._scrollYBeforeSearch ?? window.scrollY;
+        const currentScrollY = event.detail?.scrollY ?? window.scrollY;
 
         if (currentScrollY < scrollYBeforeSearch) {
-          this.toolbarVisible = true
-          if (currentScrollY > 0) this.scheduleToolbarHide()
+          this.toolbarVisible = true;
+          if (currentScrollY > 0) this.scheduleToolbarHide();
         } else if (currentScrollY > scrollYBeforeSearch) {
-          this.toolbarVisible = false
+          this.toolbarVisible = false;
         } else {
-          this.toolbarVisible = this._toolbarVisibleBeforeSearch ?? true
+          this.toolbarVisible = this._toolbarVisibleBeforeSearch ?? true;
           if (this.toolbarVisible && this._toolbarTimerWasActiveBeforeSearch) {
-            this.scheduleToolbarHide()
+            this.scheduleToolbarHide();
           }
         }
 
-        this._scrollYBeforeSearch = undefined
-        this._toolbarVisibleBeforeSearch = undefined
-        this._toolbarTimerWasActiveBeforeSearch = false
-        this._lastScrollY = currentScrollY
-        this.dispatchToolbarVisibility()
-      })
-      document.addEventListener('focusin', () => {
-        this.clearToolbarHideTimer()
-        this.toolbarVisible = true
-        this.dispatchToolbarVisibility()
-      })
-    }
-  }
+        this._scrollYBeforeSearch = undefined;
+        this._toolbarVisibleBeforeSearch = undefined;
+        this._toolbarTimerWasActiveBeforeSearch = false;
+        this._lastScrollY = currentScrollY;
+        this.dispatchToolbarVisibility();
+      });
+      document.addEventListener("focusin", () => {
+        this.clearToolbarHideTimer();
+        this.toolbarVisible = true;
+        this.dispatchToolbarVisibility();
+      });
+    },
+  };
 }
